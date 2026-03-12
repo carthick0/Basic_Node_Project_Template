1e4d2d693149725d55f760f413a7420571652cc4
pKarthikeyaaaaaaa/p <125531934+carthick0@users.noreply.github.com>
2026-03-12 20:32:34 +0530

    Add '/ping' endpoint and comment out DB connection
    Added a new '/ping' endpoint and commented out database connection code.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const connectDB=require("./config/db-config");
 const app=express();
 app.use(express.json());
 
-app.get("/",(req,res)=>{
+app.get("/ping",(req,res)=>{
     res.send("Hello World");
 });     
 
@@ -14,11 +14,11 @@ app.use("/api",apiRoutes);
 
 app.listen(PORT,async()=>{
     console.log(`Server is running on port ${PORT}`);
-    try {
-        await connectDB();
-        console.log("Connected to the database");
-    } catch (error) {
-        console.error("Database connection failed", error);
-    }
+    // try {
+    //     await connectDB();
+    //     console.log("Connected to the database");
+    // } catch (error) {
+    //     console.error("Database connection failed", error);
+    // }
     
 });
